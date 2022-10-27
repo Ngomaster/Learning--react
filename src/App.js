25e1cb4a0b21bd7c33c93226b95bf55b186f3a4e
Tran Dinh Bao <trandinhbao321@gmail.com>
2022-10-27 18:32:19 +0700

    upload

--- src/App.js
+++ src/App.js
@@ -1,23 +1,44 @@
-import logo from './logo.svg';
-import './App.css';
+import logo from "./logo.svg";
+import "./App.css";
+import Card from "./components/card";
+import Head from "./components/Header/head";
+import Sidebar from "./components/sidebar/sidebar";
+import Footer from "./components/footer/footer";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="container-parent">
+      <div className="handle-head">
+        <Head />
+      </div>
+      <div className="container">
+        <div className="handle-sidebar">
+          <Sidebar />
+        </div>
+        <div className="handle-card">
+          <div className="container">
+            <Card status="New" />
+            <Card status="Doing" />
+            <Card status="Done" />
+            <Card status="New" />
+          </div>
+          <div className="container">
+            <Card status="New" />
+            <Card status="Doing" />
+            <Card status="Done" />
+            <Card status="New" />
+          </div>
+          <div className="container">
+            <Card status="New" />
+            <Card status="Doing" />
+            <Card status="Done" />
+            <Card status="New" />
+          </div>
+        </div>
+      </div>
+      <div className="Foot">
+        <Footer />
+      </div>
     </div>
   );
 }
